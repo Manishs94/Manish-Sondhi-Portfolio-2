41129aab003c65674f5ecc434ba36762fd8349dd
Manishs94 <manishsondhi01@gmail.com>
2025-03-16 20:29:35 -0400

    Update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -93,7 +93,7 @@ export function About() {
                         rel="noopener noreferrer"
                         className="inline-flex items-center text-sm text-blue-600 hover:text-blue-800"
                       >
-                        Verify Certificate
+                        Veiw Certificate
                         <ExternalLink className="w-4 h-4 ml-1" />
                       </a>
                     </div>
